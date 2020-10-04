ab87a02ea13f2db084b1715660b21d581c387328
Juan Carlos Valdés <idevkingos@gmail.com>
2020-10-04 16:38:54 -0300

    arquitectura base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD8Q_bFcYkOliRdU_Jk2EpR4zaK3abVcvs',
+    authDomain: 'moviesapp-9566c.firebaseapp.com',
+    databaseURL: 'https://moviesapp-9566c.firebaseio.com',
+    projectId: 'moviesapp-9566c',
+    storageBucket: 'moviesapp-9566c.appspot.com',
+    messagingSenderId: '714889467891',
+    appId: '1:714889467891:web:828423b97ff5c0045fe486'
+  }
 };
 
 /*
